9eeb5951ad1e6222c92938dfc04fc592a2a56cca
xViolaine <linda.guester@yahoo.de>
2022-05-17 08:49:25 +0100

    get /api/reviews/:review_id + testing, added more error handling

--- controllers/controller-games.js
+++ controllers/controller-games.js
@@ -1,10 +1,32 @@
 const categories = require('../db/data/test-data/categories');
-const { selectCategories } = require('../models/models-games');
+const { selectCategories, selectReviewByID } = require('../models/models-games');
 
 exports.getAllCategories = (req, res, next) => {
     selectCategories()
-    .then((categories) => {
-        res.status(200).send({ categories });
-    })
-    .catch(next);
+        .then((categories) => {
+            res.status(200).send({ categories });
+        })
+        .catch(next);
 };
+
+exports.getReviewByID = (req, res, next) => {
+    const { review_id } = req.params;
+    if (isNaN(review_id)) {
+        return next({
+            status: 400,
+            msg: `'${review_id}' is not a valid review number!`,
+        });
+    }
+    selectReviewByID(review_id)
+        .then((review) => {
+            if (!review) {
+                return Promise.reject({
+                    status: 404,
+                    msg: `This review doesn't exist!`,
+                });
+            } else {
+                res.status(200).send({ review });
+            }
+        })
+        .catch(next);
+}
--- models/models-games.js
+++ models/models-games.js
@@ -5,3 +5,9 @@ exports.selectCategories = () => {
         return categories.rows;
     })
 };
+
+exports.selectReviewByID = (review_id) => {
+    return db.query(`SELECT * FROM reviews WHERE review_id = $1`, [review_id]).then((review) => {
+        return review.rows[0]
+    })
+};
