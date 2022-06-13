f45ad128cf35fc0eb1ef449ff12bbb10060d66fe
xViolaine <linda.guester@yahoo.de>
2022-06-13 16:31:04 +0100

    change endpoint

--- models/models-games.js
+++ models/models-games.js
@@ -43,7 +43,7 @@ exports.selectUsers = () => {
 exports.selectReviews = (sort_by = "created_at", order = "desc", category) => {
     let queryValues = [];
     const validSortBy = ["created_at", "owner", "category", "review_id"];
-    let queryString = `SELECT reviews.owner, reviews.review_id, reviews.created_at, reviews.title, reviews.category, reviews.review_img_url, reviews.votes, CAST (COUNT(comments) AS INTEGER) AS comment_count
+    let queryString = `SELECT reviews.owner, reviews.review_id, reviews.created_at, reviews.title, reviews.category, reviews.review_img_url, reviews.votes, reviews.review_body, CAST (COUNT(comments) AS INTEGER) AS comment_count
     FROM reviews
     LEFT JOIN comments
     ON reviews.review_id = comments.review_id`
