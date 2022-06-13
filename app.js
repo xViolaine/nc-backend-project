6e44978bc96888c84f0522e05ba89a5c4a16efc3
xViolaine <linda.guester@yahoo.de>
2022-06-13 10:31:24 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const { getAllCategories, getReviewByID, patchReviewByID, getAllUsers, getAllReviews, getCommentsByID, createComment, deleteCommentByID
 } = require('./controllers/controller-games');
 const endpoints = require('./endpoints.json')
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
