023af74df5e56939fe7d1714e049532a164b31c6
xViolaine <linda.guester@yahoo.de>
2022-06-17 12:11:59 +0100

    add category sort

--- models/models-games.js
+++ models/models-games.js
@@ -15,7 +15,8 @@ exports.selectReviewByID = (review_id) => {
     LEFT JOIN comments
     ON reviews.review_id = comments.review_id
     WHERE reviews.review_id = $1
-    GROUP BY reviews.review_id`,
+    GROUP BY reviews.review_id
+    ORDER BY created_at DESC`,
             [review_id]
         )
         .then((review) => {
